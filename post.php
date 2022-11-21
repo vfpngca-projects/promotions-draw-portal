6b2a0f37a2161bdfc892ff4642dbb98589ca8d0a
Gideon Zozingao <Gideon.Zozingao@vodafone.com.pg>
2022-11-22 09:03:05 +1000

    Edited directory access path for results.csv file

--- post.php
+++ post.php
@@ -64,7 +64,7 @@
       $count=1;
       $resultArray=array("id"=>"","number"=>"","date"=>"");
       $a1 = array();
-      $csvFile=realpath("./files/drawresults.csv");
+      $csvFile=realpath("./files/rsults/drawresults.csv");
       try {
         $fh= fopen($csvFile,"w");
          fputcsv($fh, $headers);
