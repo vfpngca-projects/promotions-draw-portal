6217d9a4fb8b39dd0a66d27bc8e2b77e13a892df
Gideon Zozingao <Gideon.Zozingao@vodafone.com.pg>
2022-11-24 15:33:59 +1000

    few more changes to the ui

--- main-view.php
+++ main-view.php
@@ -23,10 +23,10 @@
           </div>
       </div>
       <div class="lights left-ligths">
-    left
+   
       </div>
       <div class="lights right-ligths">
-right
+
       </div>
       <main class="container-fluid">
           <div class="header" id="header">
